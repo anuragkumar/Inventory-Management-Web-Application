cd841639377321ecd0d3e543dc3dab01566e5323
Anurag Kumar <kumar.anur@husky.neu.edu>
2019-04-20 21:51:30 -0400

    created inventory template

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,14 @@ import {LoginComponent} from './login/login.component';
 import {ProfileComponent} from './profile/profile.component';
 import {RegistrationComponent} from './registration/registration.component';
 import {InventoryComponent} from './inventory/inventory.component';
+import {BarcodeReaderComponent} from './barcode-reader/barcode-reader.component';
 
 const appRoutes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegistrationComponent},
   {path: 'profile', component: ProfileComponent},
-  {path: 'inventory', component: InventoryComponent}
+  {path: 'inventory', component: InventoryComponent},
+  {path: 'scan', component: BarcodeReaderComponent}
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
