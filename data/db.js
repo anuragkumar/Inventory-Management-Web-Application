1ae8ba2b4e73f1bf410ec69224054973e76d2c9c
Anurag Kumar <kumar.anur@husky.neu.edu>
2019-04-27 10:54:30 -0400

    added remote db

--- data/db.js
+++ data/db.js
@@ -2,7 +2,7 @@ module.exports = function () {
   const mongoose = require('mongoose');
   const databaseName = 'web-inventory';
   var connectionString = "mongodb://heroku_jssv03pz:lkbevsfnhsuhlll1euto665j9q@ds149056.mlab.com:49056/heroku_jssv03pz";
-  connectionString += databaseName;
+  // connectionString += databaseName;
   mongoose.connect(connectionString);
   console.log('connected');
 };
